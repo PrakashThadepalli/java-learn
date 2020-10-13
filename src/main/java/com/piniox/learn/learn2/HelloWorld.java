0c79989e66538f9b7d685d810cb28998658d90ac
srit <srit@sriprakashs-mbp.fios-router.home>
2020-10-12 21:51:20 -0400

    initial commit

---
+++ src/main/java/com/piniox/learn/learn2/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.piniox.learn.learn2;
+
+public class HelloWorld {
+    private String message;
+	public HelloWorld(String message)
+	{
+		this.message = message;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
